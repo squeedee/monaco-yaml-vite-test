6624de734096980c25cc5bad75c64e1c03c8e3ff
Rasheed Abdul-Aziz <squeedee@users.noreply.github.com>
2022-02-17 11:43:07 -0500

    No errors (without importing the root monaco-yaml)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { editor, Uri } from 'monaco-editor';
 import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-import { setDiagnosticsOptions } from 'monaco-yaml';
+// Uncommenting the following line causes worker loading errors (including for editor.worker).
+// import { setDiagnosticsOptions } from 'monaco-yaml';
 import YamlWorker from 'monaco-yaml/yaml.worker?worker';
 
 window.MonacoEnvironment = {
@@ -19,45 +20,45 @@ window.MonacoEnvironment = {
 // The uri is used for the schema file match.
 const modelUri = Uri.parse('a://b/foo.yaml');
 
-setDiagnosticsOptions({
-  enableSchemaRequest: true,
-  hover: true,
-  completion: true,
-  validate: true,
-  format: true,
-  schemas: [
-    {
-      // Id of the first schema
-      uri: 'http://myserver/foo-schema.json',
-      // Associate with our model
-      fileMatch: [String(modelUri)],
-      schema: {
-        type: 'object',
-        properties: {
-          p1: {
-            enum: ['v1', 'v2'],
-          },
-          p2: {
-            // Reference the second schema
-            $ref: 'http://myserver/bar-schema.json',
-          },
-        },
-      },
-    },
-    {
-      // Id of the first schema
-      uri: 'http://myserver/bar-schema.json',
-      schema: {
-        type: 'object',
-        properties: {
-          q1: {
-            enum: ['x1', 'x2'],
-          },
-        },
-      },
-    },
-  ],
-});
+// setDiagnosticsOptions({
+//   enableSchemaRequest: true,
+//   hover: true,
+//   completion: true,
+//   validate: true,
+//   format: true,
+//   schemas: [
+//     {
+//       // Id of the first schema
+//       uri: 'http://myserver/foo-schema.json',
+//       // Associate with our model
+//       fileMatch: [String(modelUri)],
+//       schema: {
+//         type: 'object',
+//         properties: {
+//           p1: {
+//             enum: ['v1', 'v2'],
+//           },
+//           p2: {
+//             // Reference the second schema
+//             $ref: 'http://myserver/bar-schema.json',
+//           },
+//         },
+//       },
+//     },
+//     {
+//       // Id of the first schema
+//       uri: 'http://myserver/bar-schema.json',
+//       schema: {
+//         type: 'object',
+//         properties: {
+//           q1: {
+//             enum: ['x1', 'x2'],
+//           },
+//         },
+//       },
+//     },
+//   ],
+// });
 
 const value = 'p1: \np2: \n';
 
